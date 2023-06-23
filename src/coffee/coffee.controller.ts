3c5ff85187714b252d914a10310771d3fd1c4b63
itsabubakarshf <ItsAbubakarShf@gmail.com>
2023-06-23 12:51:24 +0500

    update, delete, Query

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,13 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post,Body, HttpCode, HttpStatus, Patch, Delete, Query } from '@nestjs/common';
 
 @Controller('coffee')
 export class CoffeeController {
-    @Get()
-    findAll(){
-        return "This action returns all coffees"
-    }
     @Get(':id')
-    findOne(@Param() params){
-        return `This action returns #${params.id} coffee`
+    //Both approaches are valid
+
+    // findOne(@Param() params){
+    //     return `This action returns #${params.id} coffee`
+    // }
+    findOne(@Param('id') id: string){
+        return `This action returns #${id} coffee`
+    }
+    
+    //add coffee
+    @Post()
+    @HttpCode(HttpStatus.BAD_GATEWAY)
+    addCoffee(@Body() body:object){
+        return body
+    }
+    
+    //update coffee
+    @Patch(':id')
+    updateCoffee(@Param('id') id: string, @Body() body:object){
+        return 'This action updates a coffee with id #' + id + ' and body ' + JSON.stringify(body)
+    }
+
+    //delete coffee
+    @Delete(':id')
+    deleteCoffee(@Param('id') id: string, @Body() body:object){
+        return 'This action delete a coffee with id #' + id + ' and body ' + JSON.stringify(body)
+    }
+
+    //use of Query:paginationQuery
+    @Get()
+    findAll(@Query() paginationQuery){
+        const {limit, offset}=paginationQuery;
+        return `The limit is ${limit} and offet is ${offset}`
     }
 }
