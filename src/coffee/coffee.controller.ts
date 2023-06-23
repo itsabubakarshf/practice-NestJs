ed308d4691823ceb7d77f9c7d639bd359bb220b2
itsabubakarshf <ItsAbubakarShf@gmail.com>
2023-06-23 12:03:59 +0500

    get all and specifc coffee

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffee')
-export class CoffeeController {}
+export class CoffeeController {
+    @Get()
+    findAll(){
+        return "This action returns all coffees"
+    }
+    @Get(':id')
+    findOne(@Param() params){
+        return `This action returns #${params.id} coffee`
+    }
+}
